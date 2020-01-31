615e4a851afab532094256d79953a241b13f1e37
Daniel Freedman <dfreedm@google.com>
2020-01-30 17:49:25 -0800

    Refactor checkbox to inline DOM manipulations
    - Move all the DOM manipulations from MDCCheckboxFoundation into the
      template
    - Make Foundation and Adapter generation optional in
      mwc-base/BaseElement

--- packages/checkbox/src/mwc-checkbox-base.ts
+++ packages/checkbox/src/mwc-checkbox-base.ts
@@ -14,65 +14,93 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import {MDCCheckboxAdapter} from '@material/checkbox/adapter.js';
-import MDCCheckboxFoundation from '@material/checkbox/foundation.js';
-import {addHasRemoveClass, FormElement, HTMLElementWithRipple, observer} from '@material/mwc-base/form-element.js';
+import {FormElement, HTMLElementWithRipple} from '@material/mwc-base/form-element.js';
 import {rippleNode} from '@material/mwc-ripple/ripple-directive.js';
-import {html, property, query} from 'lit-element';
+import {html, property, PropertyValues, query} from 'lit-element';
+import {classMap} from 'lit-html/directives/class-map.js';
+import {ifDefined} from 'lit-html/directives/if-defined.js';
 
 export class CheckboxBase extends FormElement {
   @query('.mdc-checkbox') protected mdcRoot!: HTMLElementWithRipple;
 
   @query('input') protected formElement!: HTMLInputElement;
 
-  @property({type: Boolean}) checked = false;
+  @property({type: Boolean, reflect: true}) checked = false;
 
   @property({type: Boolean}) indeterminate = false;
 
-  @property({type: Boolean})
-  @observer(function(this: CheckboxBase, value: boolean) {
-    this.mdcFoundation.setDisabled(value);
-  })
-  disabled = false;
+  @property({type: Boolean, reflect: true}) disabled = false;
 
   @property({type: String}) value = '';
 
-  protected mdcFoundationClass = MDCCheckboxFoundation;
+  @property({type: String}) name = '';
 
-  protected mdcFoundation!: MDCCheckboxFoundation;
+  // use @internalProperty() when published
+  @property({attribute: false}) protected animationClass = '';
+
+  // MDC Foundation is not used
+  protected createAdapter() {
+    return {};
+  }
+
+  protected mdcFoundation = undefined;
+
+  protected mdcFoundationClass = undefined;
 
   get ripple() {
     return this.mdcRoot.ripple;
   }
 
-  protected createAdapter(): MDCCheckboxAdapter {
-    return {
-      ...addHasRemoveClass(this.mdcRoot),
-      forceLayout: () => {
-        this.mdcRoot.offsetWidth;
-      },
-      isAttachedToDOM: () => this.isConnected,
-      isIndeterminate: () => this.indeterminate,
-      isChecked: () => this.checked,
-      hasNativeControl: () => Boolean(this.formElement),
-      setNativeControlDisabled: (disabled: boolean) => {
-        this.formElement.disabled = disabled;
-      },
-      setNativeControlAttr: (attr: string, value: string) => {
-        this.formElement.setAttribute(attr, value);
-      },
-      removeNativeControlAttr: (attr: string) => {
-        this.formElement.removeAttribute(attr);
-      },
-    };
+  protected update(changedProperties: PropertyValues) {
+    const oldIndeterminate = changedProperties.get('indeterminate');
+    const oldChecked = changedProperties.get('checked');
+    if (oldIndeterminate !== undefined || oldChecked !== undefined) {
+      const oldState =
+          this.calculateAnimationStateName(!!oldChecked, !!oldIndeterminate);
+      const newState =
+          this.calculateAnimationStateName(this.checked, this.indeterminate);
+      this.animationClass = `${oldState}-${newState}`;
+    }
+    super.update(changedProperties);
+  }
+
+  protected calculateAnimationStateName(
+      checked: boolean, indeterminate: boolean): string {
+    if (indeterminate) {
+      return 'indeterminate';
+    } else if (checked) {
+      return 'checked';
+    } else {
+      return 'unchecked';
+    }
   }
 
   protected render() {
+    /** @classMap */
+    const classes = {
+      'mdc-checkbox--disabled': this.disabled,
+      'mdc-checkbox--selected': this.indeterminate || this.checked,
+      // transition animiation classes
+      'mdc-checkbox--anim-checked-indeterminate':
+          this.animationClass === 'checked-indeterminate',
+      'mdc-checkbox--anim-checked-unchecked':
+          this.animationClass === 'checked-unchecked',
+      'mdc-checkbox--anim-indeterminate-checked':
+          this.animationClass === 'indeterminate-checked',
+      'mdc-checkbox--anim-indeterminate-unchecked':
+          this.animationClass === 'indeterminate-unchecked',
+      'mdc-checkbox--anim-unchecked-checked':
+          this.animationClass === 'unchecked-checked',
+      'mdc-checkbox--anim-unchecked-indeterminate':
+          this.animationClass === 'unchecked-indeterminate',
+    };
     return html`
-      <div class="mdc-checkbox"
-           @animationend="${this._animationEndHandler}">
+      <div class="mdc-checkbox mdc-checkbox--upgraded ${classMap(classes)}">
         <input type="checkbox"
               class="mdc-checkbox__native-control"
+              aria-checked="${
+        ifDefined(this.indeterminate ? 'mixed' : undefined)}"
+              ?disabled="${this.disabled}"
               @change="${this._changeHandler}"
               .indeterminate="${this.indeterminate}"
               .checked="${this.checked}"
@@ -90,19 +118,14 @@ export class CheckboxBase extends FormElement {
       </div>`;
   }
 
+  private _changeHandler() {
+    this.checked = this.formElement.checked;
+    this.indeterminate = this.formElement.indeterminate;
+  }
+
   firstUpdated() {
     super.firstUpdated();
     this.mdcRoot.ripple = rippleNode(
         {surfaceNode: this.mdcRoot, interactionNode: this.formElement});
-  }
-
-  private _changeHandler() {
-    this.checked = this.formElement.checked;
-    this.indeterminate = this.formElement.indeterminate;
-    this.mdcFoundation.handleChange();
-  }
-
-  private _animationEndHandler() {
-    this.mdcFoundation.handleAnimationEnd();
   }
 }
